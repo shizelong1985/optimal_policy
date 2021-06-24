d9d78d88b92ecb3547056c1554ac11365c4ce873
Michael Cai <michaeldcai@gmail.com>
2021-06-24 09:37:20 -0500

    Discover cause of pol_pi_shock method of forward_step_shock not working. It's because a_i are changing from iteration to iteration

--- scripts/asymp_disc_sums.py
+++ scripts/asymp_disc_sums.py
@@ -1,5 +1,6 @@
 """A script for implementing the asymptotic discounted sums needed for SS of the optimum calculations"""
 
+import copy
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,16 +13,63 @@ import src.asymp_disc_sums as ads
 ss = sim.steady_state(**sim.example_calibration())
 
 T = 300
-shocks = {"r": 1}  # Here, ex-post r
+# h = 1e-4
+h = 1e-5
+shocks = {'r': 1.}  # Here, ex-post r
 
-curlyYs, curlyDs = fn.get_curlyYs_curlyDs(ss, T, shocks)
+curlyYs, curlyDs = fn.get_curlyYs_curlyDs(ss, T, shocks, h=h)
 curlyEs = fn.get_curlyEs(ss, T)
 
+# New way of getting cumulative sums of curlyYs, curlyDs, and curlyEs, solving for tolerances as opposed to
+# specifying a given T ex-ante
+inputs = ['Va', 'Pi', 'a_grid', 'y', 'r', 'beta', 'eis']
+outputs = ['A', 'C']
+back_iter_vars = ['Va']
+back_iter_outputs = ['Va', 'a', 'c']
+policy = ['a']
+# shocked_inputs = {'r': ss['r'] + h}
+shocked_inputs = {'r': 1.}
+policy_repr = ads.get_sparse_policy_repr(ss, policy)
+outputs_ss_vals = sim.backward_iteration(**{i: ss[i] for i in inputs})
+
 # The T - 1 shocked inputs
-h = 1e-4
-shocked_inputs = {"r": ss["r"] + h}
-curlyYs_Tm1, curlyDs_Tm1 = ads.get_curlyYs_curlyDs_single_step(ss, shocked_inputs, h=h)
+curlyVs_Tm1, curlyDs_Tm1, curlyYs_Tm1 = ads.backward_step_fakenews(ss, inputs, outputs, back_iter_vars, back_iter_outputs,
+                                                                   policy, shocked_inputs, h=h,
+                                                                   policy_repr=policy_repr, outputs_ss_vals=outputs_ss_vals)
+
+# The T - 2 shocked inputs
+ss_Tm2 = copy.deepcopy(ss)
+ss_Tm2["Va"] = ss["Va"] + curlyVs_Tm1["Va"] * h
+shocked_inputs_Tm2 = {}  # Probably a more efficient way to implement in the code to not calculate a shock
+curlyVs_Tm2, curlyDs_Tm2, curlyYs_Tm2 = ads.backward_step_fakenews(ss_Tm2, inputs, outputs, back_iter_vars, back_iter_outputs,
+                                                                   policy, {}, h=h,
+                                                                   policy_repr=policy_repr, outputs_ss_vals=outputs_ss_vals)
+
+curlyDs_comp_Tm1 = curlyDs["r"][0]
+curlyDdiff_Tm1 = curlyDs_Tm1 - curlyDs_comp_Tm1
+curlyDmaxdiffs_Tm1 = np.max(np.abs(curlyDdiff_Tm1), axis=1)
+
+curlyDs_comp_Tm2 = curlyDs["r"][1]
+curlyDdiff_Tm2 = curlyDs_Tm2 - curlyDs_comp_Tm2
+curlyDmaxdiffs_Tm2 = np.max(np.abs(curlyDdiff_Tm2), axis=1)
 
 assert np.isclose(curlyYs_Tm1["A"], curlyYs["A"]["r"][0])
 assert np.isclose(curlyYs_Tm1["C"], curlyYs["C"]["r"][0])
 assert np.all(np.isclose(curlyDs_Tm1, curlyDs["r"][0]))
+
+assert np.isclose(curlyYs_Tm2["A"], curlyYs["A"]["r"][1])
+assert np.isclose(curlyYs_Tm2["C"], curlyYs["C"]["r"][1])
+assert np.all(np.isclose(curlyDs_Tm2, curlyDs["r"][1]))
+
+aaa
+for i in range(len(ss["y"])):
+    plt.plot(curlyDdiff[i, :])
+plt.show()
+
+for i in range(len(ss["y"])):
+    plt.plot(curlyDs_Tm1[i, :])
+plt.show()
+
+for i in range(len(ss["y"])):
+    plt.plot(curlyDs["r"][0][i, :])
+plt.show()
--- src/asymp_disc_sums.py
+++ src/asymp_disc_sums.py
@@ -2,48 +2,65 @@
 
 import numpy as np
 
+import sequence_jacobian.utilities as utils
 import src.ssj_template_code.standard_incomplete_markets as sim
 
+# Most of the below code is borrowed and modified slightly from the HetBlock code, s.t. the optimal_policy repo only
+# relies on SSJ utilities for now, as opposed to the entire code infrastructure.
 
-# Re-write policy_impulse_response to be a single backward iteration step
+
+def get_sparse_policy_repr(ss, policy):
+    grid = {k: ss[k + '_grid'] for k in policy}
+    # Get sparse representations of policy rules, and distance between neighboring policy gridpoints
+    sspol_i = {}
+    sspol_pi = {}
+    sspol_space = {}
+    for pol in policy:
+        # use robust binary-search-based method that only requires grids to be monotonic
+        sspol_i[pol], sspol_pi[pol] = utils.interpolate.interpolate_coord_robust(grid[pol], ss[pol])
+        sspol_space[pol] = grid[pol][sspol_i[pol]+1] - grid[pol][sspol_i[pol]]
+    return {"pol_i": sspol_i, "pol_pi": sspol_pi, "pol_space": sspol_space}
+
+
 # Need to be careful about setting `h`, since the initial `h`-scaled shock is not be computed in this current step,
 # compared to the structure of the old get_curlyYs_curlyDs function.
 # Hence, when calculating curlyYs and curlyDs w/ resp. to h, it needs to be the same `h` from which the initial `h`-scaled shock
 # was calculated.
-def get_curlyYs_curlyDs_single_step(ss, shocked_inputs,
-                                    inputs=['a_grid', 'y', 'r', 'beta', 'eis', 'Va', 'Pi'], h=1e-4):
-    input_args_noshock = {k: ss[k] for k in inputs}
-    input_args = input_args_noshock.copy()
+def backward_step_fakenews(ss, inputs, outputs, back_iter_vars, back_iter_outputs, policy, shocked_inputs, h=1e-4,
+                           policy_repr=None, outputs_ss_vals=None):
+    if policy_repr is None:
+        policy_repr = get_sparse_policy_repr(ss, policy)
 
-    for k in [j for j in shocked_inputs if j in inputs]:
-        input_args[k] = shocked_inputs[k]
+    # shock perturbs outputs
+    shocked_outputs = {k: v for k, v in zip(back_iter_outputs, utils.differentiate.numerical_diff(sim.backward_iteration,
+                                                                                        {j: ss[j] for j in inputs},
+                                                                                        shocked_inputs, h,
+                                                                                        outputs_ss_vals))}
+    curlyV = {k: shocked_outputs[k] for k in back_iter_vars}
 
-    # initialize dicts of curlyYs[o][i] and curlyDs[i]
-    curlyYs = {}
-    for o in ['A', 'C']:  # TODO: Later generalize beyond the simple SIM case...
-        curlyYs[o] = {}
+    # which affects the distribution tomorrow
+    p = next(iter(policy))  # For the 1d case, `policy` should be a singleton list
+    pol_pi_shock = -shocked_outputs[p]/policy_repr["pol_space"][p]
+    # TODO: Test, manually construct pol_pi_shock
+    #   pol_pi_shock matches pol_pi_shock_manual, except for a few entries that seem to be due to numerical
+    #   differentiation error (the discrepancy is of order 1/h in these entries)
+    a_i_old, a_pi_old = sim.get_lottery(ss["a"], ss["a_grid"])
+    # a_i_new, a_pi_new = sim.get_lottery(ss["a"] + shocked_outputs["a"] * h, ss["a_grid"])
+    # pol_pi_shock_manual = (a_pi_new - a_pi_old)/h
+    a_i_new, a_pi_new = sim.get_lottery(ss["a"] + shocked_outputs["a"] * h, ss["a_grid"])
+    pol_pi_shock_manual = (a_pi_new - a_pi_old)/h
 
-    # start with "ghost run": what happens without any shocks?
-    _, a_noshock, c_noshock = sim.backward_iteration(**input_args_noshock)
-    D_noshock = one_period_ahead_single_step(ss, a_noshock)
+    curlyD = utils.forward_step.forward_step_shock_1d(ss["D"], ss["Pi"].T.copy(), policy_repr["pol_i"][p],
+                                                      # -shocked_outputs[p]/policy_repr["pol_space"][p])
+                                                      pol_pi_shock)
+    # TODO: a_i_new \neq a_i_old!! So might be some weirdness going on when using pol_pi_shock!
+    # # Matches the curlyD calculation in SIM
+    # curlyD = (utils.forward_step.forward_step_1d(ss["D"], ss["Pi"].T.copy(), a_i_new, a_pi_new) -\
+    #           utils.forward_step.forward_step_1d(ss["D"], ss["Pi"].T.copy(), a_i_old, a_pi_old)) / h
+    # Matches forward_step in SIM (doing D instead of (D - D_noshock)/h)
+    # curlyD = utils.forward_step.forward_step_1d(ss["D"], ss["Pi"].T.copy(), a_i_new, a_pi_new)
 
-    # compute the actual policy response to the shocks
-    _, a, c = sim.backward_iteration(**input_args)
-    D = one_period_ahead_single_step(ss, a)
+    # and the aggregate outcomes today
+    curlyY = {k: np.vdot(ss["D"], shocked_outputs[k.lower()]) for k in outputs}
 
-    # aggregate using steady-state distribution to get curlyYs
-    curlyYs['A'] = np.vdot(ss['D'], (a - a_noshock)) / h
-    curlyYs['C'] = np.vdot(ss['D'], (c - c_noshock)) / h
-
-    curlyDs = (D - D_noshock) / h
-
-    return curlyYs, curlyDs
-
-
-def one_period_ahead_single_step(ss, a):
-    """If a[t] is (shocked) asset policy at time t, find distribution
-    D[t+1], assuming D[t] is the steady-state distribution"""
-
-    a_i, a_pi = sim.get_lottery(a, ss['a_grid'])
-    D = sim.forward_iteration(ss['D'], ss['Pi'], a_i, a_pi)
-    return D
+    return curlyV, curlyD, curlyY
